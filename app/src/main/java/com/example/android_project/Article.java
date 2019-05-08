8a35932b4109fc6bf4dae6e3a9cb9665d1fb0cbd
ElliotLEdwards <elliot.edwards38@gmail.com>
2019-05-08 21:37:24 +0200

    Post and Get functionnalities

--- app/src/main/java/com/example/android_project/Article.java
+++ app/src/main/java/com/example/android_project/Article.java
@@ -3,24 +3,32 @@ package com.example.android_project;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
 
 public class Article extends AppCompatActivity {
 
     public String title;
     public String description;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_article);
+        Button button= (Button) findViewById(R.id.addButton);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                EditText titleInput = (EditText) findViewById(R.id.titleInput);
+                EditText descriptionInput = (EditText) findViewById(R.id.descriptionInput);
+
+                Toast.makeText(getApplicationContext(),titleInput.getText() + " + " + descriptionInput.getText(),Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
--- app/src/main/java/com/example/android_project/ArticleAsyncTaskParse.java
+++ app/src/main/java/com/example/android_project/ArticleAsyncTaskParse.java
@@ -14,39 +14,12 @@ public class ArticleAsyncTaskParse extends AsyncTask<Object, Void, Void> {
     @Override
     protected Void doInBackground(Object... objects) {
         try{
-
-            String dateDebut = (String) objects[0];
-            String dateFin = (String) objects[1];
-            planets = (ArrayList<Planet>) objects[2];
-            adapter = (PlanetAdapter) objects[3];
-            String lien ="https://api.nasa.gov/planetary/apod?start_date="+ dateDebut +"&end_date="+ dateFin +"&api_key=tTVoD66Uy47eCyMdHv94Q1EDsw2dPKOsjAO6vr2G";
-            URL url = new URL(lien);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.connect();
             InputStream inputStream = connection.getInputStream();
-            String result = InputStreamToString(inputStream);
-            JSONArray array = new JSONArray(result);
 
 
 
-            for(int i=0; i<array.length(); i++)
-            {
-                JSONObject obj = new JSONObject(array.getString(i));
-
-                String title = obj.getString("title");
-
-
-                Planet planet = new Planet();
-
-                planet.setTitle(obj.getString("title"));
-                planet.setDate(obj.getString("date"));
-                planet.setDescription(obj.getString("explanation"));
-                planet.setImageUrl(obj.getString("url"));
-                planet.setMediaType(obj.getString("media_type"));
-
-                planets.add(planet);
-
-            }
         }catch(Exception e){
             e.printStackTrace();
         }
